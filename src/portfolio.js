3c1913009bb2282e25cfc3d3ce3576a0d72942fc
Servcon <moncefservcon@gmail.com>
2025-05-05 10:24:45 +0300

    email number change

--- src/portfolio.js
+++ src/portfolio.js
@@ -356,8 +356,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+974 66900812",
+  email_address: "moncef@servcon.qa"
 };
 
 // Twitter Section
